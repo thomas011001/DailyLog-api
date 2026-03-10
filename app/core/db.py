4f551b5846de965a1e3410dd6ffe804e5a197728
thomas011001 <thomasyacoub321@gmail.com>
2026-03-10 20:26:35 +0200

    Remove unnecessary connect_args from database engine creation

--- app/core/db.py
+++ app/core/db.py
@@ -10,8 +10,8 @@ load_dotenv()
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "")
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+    SQLALCHEMY_DATABASE_URL
+)   
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
