09b7a4729ff9e8aa6f7ef584082166e45f466d01
thomas011001 <thomasyacoub321@gmail.com>
2026-03-11 01:40:52 +0200

    Implement login functionality with JWT token generation

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,13 +1,21 @@
+import datetime
+import os
+
+from dotenv import load_dotenv
 from fastapi import APIRouter, Depends, HTTPException, status
+import jwt
 from pydantic import BaseModel, Field
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session
 from app.core.db import get_db
+from app.models import User
 from app.repo.user import UserRepo
-from app.utiles import hash_password
+from app.utiles import hash_password, verify_password
+
+load_dotenv()
 
 # sign up [V]
-# login [ ]
+# login [V]
 # logout [ ]
 # refresh [ ]
 
@@ -18,6 +26,10 @@ class CreateUser(BaseModel):
 
 class UserOut(BaseModel):
   username: str
+
+class Credentials(BaseModel):
+  username:str = Field(min_length=1)
+  password:str = Field(min_length=1)
 
 auth_router = APIRouter()
 
@@ -42,3 +54,22 @@ def create_user(user: CreateUser, db: Session = Depends(get_db)):
       
   return new_user
   
+@auth_router.post("/login", status_code=status.HTTP_200_OK)
+def login(credentials: Credentials, db:Session = Depends(get_db)):
+  repo = UserRepo(db)
+  user = repo.get_user_by_username(credentials.username)
+  if not user:
+    raise HTTPException(status.HTTP_400_BAD_REQUEST, "Invalid username or password")
+  
+  correct_password = verify_password(credentials.password, user.password_hash)
+  if not correct_password:
+    raise HTTPException(status.HTTP_400_BAD_REQUEST, "Invalid username or password")
+  
+  payload = {
+    "id": user.id,
+    "username": user.username,
+    "exp": datetime.datetime.utcnow() + datetime.timedelta(hours=48) 
+  }
+  token = jwt.encode(payload, os.getenv("ACCESS_TOKEN_KEY", "very secret key"))
+
+  return {"token": token}
