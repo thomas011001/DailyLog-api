bad4dae725ba1f4fc1526d5a6097cdf98a9836e9
thomas011001 <thomasyacoub321@gmail.com>
2026-03-14 01:43:46 +0200

    Implement UserRepo class for user management: create user, check username existence, and retrieve user by username

--- app/repo/user_repo.py
+++ app/repo/user_repo.py
@@ -7,12 +7,12 @@ class UserRepo:
   def __init__(self, db: Session):
     self.db = db
 
-  def create_user(self, user_data: dict):
-    user = User(**user_data)
-    self.db.add(user)
+  def create_user(self, user):
+    new_user = user
+    self.db.add(new_user)
     self.db.commit()
-    self.db.refresh(user)
-    return user
+    self.db.refresh(new_user)
+    return new_user
   
   def username_exists(self, username):
     return True if self.db.query(User).filter(User.username == username).first() else False
