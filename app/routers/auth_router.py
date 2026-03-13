ca3626e68acb46876dd7d62558070c07cd959b60
thomas011001 <thomasyacoub321@gmail.com>
2026-03-14 01:43:34 +0200

    Implement user authentication routes: signup and login functionality

--- app/routers/auth_router.py
+++ app/routers/auth_router.py
@@ -8,8 +8,9 @@ from pydantic import BaseModel, Field
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session
 from app.core.db import get_db
+from app.dependancies import get_user_repo
 from app.models import User
-from app.repo.user import UserRepo
+from app.repo.user_repo import UserRepo
 from app.utiles import hash_password, verify_password
 
 load_dotenv()
@@ -25,6 +26,7 @@ class CreateUser(BaseModel):
   password: str = Field(min_length=5) 
 
 class UserOut(BaseModel):
+  id: int
   username: str
 
 class Credentials(BaseModel):
@@ -34,29 +36,22 @@ class Credentials(BaseModel):
 auth_router = APIRouter()
 
 @auth_router.post("/signup", response_model=UserOut, status_code=status.HTTP_201_CREATED)
-def create_user(user: CreateUser, db: Session = Depends(get_db)):
-  repo = UserRepo(db)
+def create_user(user: CreateUser, repo: UserRepo = Depends(get_user_repo)):
 
-  # if repo.username_exists(user.username):
-  #   raise HTTPException(status.HTTP_409_CONFLICT, "This username is taken .")
-  
   hashed_password = hash_password(user.password)
 
   try:
-    new_user = repo.create_user({"username": user.username, "password_hash": hashed_password})
-
+    new_user = repo.create_user(User(username=user.username, password_hash=hashed_password))
   except IntegrityError:
-    raise HTTPException(status.HTTP_409_CONFLICT, "This username is taken .")
+    raise HTTPException(status.HTTP_409_CONFLICT, "This username is taken.")
 
   except Exception:
-      db.rollback()
       raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR, "Could not create the user.")
       
   return new_user
   
 @auth_router.post("/login", status_code=status.HTTP_200_OK)
-def login(credentials: Credentials, db:Session = Depends(get_db)):
-  repo = UserRepo(db)
+def login(credentials: Credentials, repo: UserRepo = Depends(get_user_repo)):
   user = repo.get_user_by_username(credentials.username)
   if not user:
     raise HTTPException(status.HTTP_400_BAD_REQUEST, "Invalid username or password")
@@ -65,11 +60,6 @@ def login(credentials: Credentials, db:Session = Depends(get_db)):
   if not correct_password:
     raise HTTPException(status.HTTP_400_BAD_REQUEST, "Invalid username or password")
   
-  payload = {
-    "id": user.id,
-    "username": user.username,
-    "exp": datetime.datetime.utcnow() + datetime.timedelta(hours=48) 
-  }
-  token = jwt.encode(payload, os.getenv("ACCESS_TOKEN_KEY", "very secret key"))
+  token = user.token
 
   return {"token": token}
